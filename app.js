efb98ef7cae6cb22a47cf33ce16e42ee7b15d106
cratuss <ldoh92@gmail.com>
2022-08-11 00:09:23 +0900

    [ADD] 유저 정보 (게시물 목록 포함) 엔드포인트ver1.1

--- app.js
+++ app.js
@@ -14,152 +14,141 @@ const users = [
     email: "Connell29@gmail.com",
     password: "password",
   },
+  {
+    id: 3,
+    name: "warrent Predovic",
+    email: "warrent@gmail.com",
+    password: "password",
+  },
+  {
+    id: 4,
+    name: "ldoh92 Predovic",
+    email: "ldoh92@gmail.com",
+    password: "password",
+  },
 ];
 
 //게시물 데이터 - 1
 const posts = [
   {
-    id: 1,
-    title: "간단한 HTTP API 개발 시작!",
-    content: "Node.js에 내장되어 있는 http 모듈을 사용해서 HTTP server를 구현.",
+    postingId: 1,
+    postingTitle: "간단한 HTTP API 개발 시작!",
+    postingContent: "Node.js에 내장되어 있는 http 모듈을 사용해서 HTTP server를 구현.",
     userId: 1,
   },
   {
-    id: 2,
-    title: "HTTP의 특성",
-    content: "Request/Response와 Stateless!!",
+    postingId: 2,
+    postingTitle: "HTTP의 특성",
+    postingContent: "Request/Response와 Stateless!!",
     userId: 1,
   },
+  {
+    postingId: 3,
+    postingTitle: "내용 1",
+    postingContent: "sampleContent3",
+    userId: 3,
+  },
+  {
+    postingId: 4,
+    postingTitle: "내용 2",
+    postingContent: "sampleContent4",
+    userId: 4,
+  },
 ];
-
-//게시글 목록 데이터 - 2
-const list = {
-  data: [
-    {
-      userID: 1,
-      userName: "Rebekah Johnson",
-      postingId: 1,
-      postingTitle: "간단한 HTTP API 개발 시작!",
-      postingContent: "Node.js에 내장되어 있는 http 모듈을 사용해서 HTTP server를 구현.",
-    },
-    {
-      userID: 2,
-      userName: "Fabian Predovic",
-      postingId: 2,
-      postingTitle: "HTTP의 특성",
-      postingContent: "Request/Response와 Stateless!!",
-    },
-    {
-      userID: 3,
-      userName: "new user 1",
-      postingId: 3,
-      postingTitle: "내용 1",
-      postingContent: "sampleContent3",
-    },
-    {
-      userID: 1,
-      userName: "new user 2",
-      postingId: 4,
-      postingTitle: "내용 2",
-      postingContent: "sampleContent4",
-    },
-  ],
-};
 
 // 아래에 코드를 작성해 주세요.
 
 //회원가입 구현
 const addUser = (req, res) => {
-  const { id, name, email, password } = req.body.userinfo;
+  const { name, email, password } = req.body.data;
+  if (users.length === 0) {
+    users.push({ id: 1, name, email, password });
+  } else {
+    users.push({ id: users.length + 1, name, email, password });
+  }
 
-  users.push({ id, name, email, password });
-
-  console.log(users);
-  res.status(201);
-  res.json({ message: "userCreated" });
+  res.status(201).json({ message: "userCreated" });
 };
 
 //게시글 생성 구현
 const addPost = (req, res) => {
-  const { id, title, content, userId } = req.body.postinfo;
+  const { postingTitle, postingContent, userId } = req.body.data;
 
-  posts.push({ id, title, content, userId });
+  if (posts.length === 0) {
+    posts.push({ postingId: 1, postingTitle, postingContent, userId });
+  } else {
+    posts.push({ postingId: posts.length + 1, postingTitle, postingContent, userId });
+  }
 
-  console.log(posts);
-  res.status(201);
-  res.json({ message: "postCreated" });
+  res.status(201).json({ message: "postCreated" });
 };
 
 //게시글 목록 출력
 const postList = (req, res) => {
-  res.status(200);
-  res.json(list);
+  let newPosts = posts.map((post) => {
+    const user = users.find((user) => post.userId === user.id);
+    return {
+      ...post,
+      userName: user.name,
+    };
+  });
+
+  res.status(200).json({ data: newPosts });
 };
 
 //게시글 수정
 const editPost = (req, res) => {
-  const { postingId } = req.params;
-  const { userId, userName, postingTitle, postingContent } = req.body.data;
+  const { id, content } = req.body.data;
 
-  const resEdit = {};
-
-  list.data.some((element) => {
-    if (element.postingId === Number(postingId)) {
-      element.userID = userId;
-      element.userName = userName;
-      element.postingTitle = postingTitle;
-      element.postingContent = postingContent;
-      resEdit.data = element;
+  posts.some((post) => {
+    if (post.postingId === Number(id)) {
+      const user = users.find((user) => user.id === post.userId);
+      post.postingContent = content;
+      post.userName = user.name;
+      res.status(201).json({ data: post });
       return true;
     }
     return false;
   });
-
-  res.status(201);
-  res.json(resEdit);
 };
 
 //게시글 삭제
 const deletePost = (req, res) => {
-  const { postingId } = req.params;
-  for (let i = 0; i < list.data.length; i++) {
-    if (list.data[i].postingId === Number(postingId)) {
-      list.data.splice(i, 1);
+  const { id } = req.body.data;
+  for (let i = 0; i < posts.length; i++) {
+    if (posts[i].postingId === Number(id)) {
+      posts.splice(i, 1);
       break;
     }
   }
-  console.log(list.data);
-  res.status(201);
-  res.json({ message: "postingDeleted" });
+  console.log(posts);
+  res.status(201).json({ message: "postingDeleted" });
 };
 
+//해당 유저 게시글 확인
 const userPosting = (req, res) => {
-  const { userId } = req.params;
+  const { id } = req.params;
 
-  const resUserList = {};
-  resUserList.data = {};
-
-  users.some((element) => {
-    if (element.id === Number(userId)) {
-      resUserList.data.userID = element.id;
-      resUserList.data.userName = element.name;
-      return true;
-    }
-  });
-
-  resUserList.data.postings = [];
-  list.data.forEach((element) => {
-    if (element.userID === Number(userId)) {
-      resUserList.data.postings.push({
-        postingId: element.postingId,
-        postingTitle: element.postingTitle,
-        postingContent: element.postingContent,
+  const postings = [];
+  for (let i = 0; i < posts.length; i++) {
+    if (posts[i].userId === Number(id)) {
+      const { userId, ...rest } = posts[i];
+      postings.push({
+        ...rest,
       });
     }
-  });
+  }
+
+  const user = users.find((user) => user.id === Number(id));
+
+  const newUserPosting = {
+    userID: user.id,
+    userName: user.name,
+    postings,
+  };
 
   res.status(200);
-  res.json(resUserList);
+  res.json({ data: newUserPosting });
 };
 
 module.exports = { addUser, addPost, postList, editPost, deletePost, userPosting };
